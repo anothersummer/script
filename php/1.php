ec91de7ee9222cd0f99a568494a952ee4a0996a5
summer <706778074@qq.com>
2015-11-10 14:00:08 +0800

    Update 1.php

--- php/1.php
+++ php/1.php
@@ -1,1 +1,25 @@
+<?php
+$arr = array(
+    'arr0' => array(
+            'crr0' => array(
+                'c01' => 1,
+                'c02' => 2,
+               
+            ),
+            'crr1' => array(
+                'c11' => 1,
+                'c12' => 2,
+               
+            ),
+    )
+);
+
+//输出:
+// arr0 crr0 c01 1
+// arr0 crr0 c02 2
+// arr0 crr1 c11 1
+// arr0 crr1 c12 2
+
+// 怎么实现?
+
 
